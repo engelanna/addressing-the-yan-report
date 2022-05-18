e58877063ee067145be96d4812183bfe143a1991
Anna Julia Engel anna.julia.engel@gmail.com <anna.julia.engel@gmail.com>
2022-05-18 04:32:13 +0200

    Raise if input FASTA is multiple-sequence (+test)

--- src/loaders/sole_sequence_from_fasta_file.py
+++ src/loaders/sole_sequence_from_fasta_file.py
@@ -1,9 +1,15 @@
 class SoleSequenceFromFastaFile:
     def __call__(self, file_path: str):
-        the_sequence = str()
+
+        the_sequence = ""
+        header_already_encountered = False
 
         for line in open(file_path, "r"):
-            if not line.startswith(">"):
+            if line.startswith(">") and header_already_encountered:
+                raise AssertionError("Only 1 sequence allowed in the FASTA file")
+            elif line.startswith(">"):
+                header_already_encountered = True
+            else:
                 the_sequence += line.rstrip()
 
         return the_sequence
--- test/loaders/test_sole_sequence_from_fasta_file.py
+++ test/loaders/test_sole_sequence_from_fasta_file.py
@@ -1,10 +1,19 @@
+from pytest import raises
 from src.loaders import SoleSequenceFromFastaFile
 
 
 class TestSoleSequenceFromFastaFile:
-    def test_length_of_the_only_sequence(self, expected_length=29903):
-        actual_length = len(
-            SoleSequenceFromFastaFile()("assets/fasta/sars_cov_2_nc_045512.2.fasta")
-        )
+    def test_length_of_the_only_sequence(
+        self,
+        file_path="assets/fasta/sars_cov_2_nc_045512.2.fasta",
+        expected_length=29903,
+    ):
+        actual_length = len(SoleSequenceFromFastaFile()(file_path))
 
         assert expected_length == actual_length
+
+    def test_prohibit_multiple_fasta_sequences(
+        self, file_path="assets/fasta/illegal.fasta"
+    ):
+        with raises(AssertionError, match="Only 1 sequence allowed in the FASTA file"):
+            SoleSequenceFromFastaFile()(file_path)
