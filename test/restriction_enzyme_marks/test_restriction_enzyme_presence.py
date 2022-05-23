b26c498f45c9d8ac9e660d29b94b5f46a464877e
Anna Julia Engel <anna.julia.engel@gmail.com>
2022-05-23 10:53:03 +0200

    Add tests for enzyme presence within tolerance

--- test/restriction_enzyme_marks/test_restriction_enzyme_presence.py
+++ test/restriction_enzyme_marks/test_restriction_enzyme_presence.py
@@ -6,25 +6,19 @@ from src.restriction_enzyme_marks import RestrictionEnzymePresence
 
 class TestRestrictionEnzymePresence:
     @fixture
-    def mock_restriction_enzyme(self):
+    def ecori(self):
         return RestrictionEnzyme(
-            start_at_fraction_genome_length=0.5,
-            name="Not a real restriction enzyme, but good enough for test purposes",
-            sequence="CATNNNNNNACT",
+            start_at_fraction_genome_length=0.06,  # actually at 3.8% (not 6%)
+            name="EcoRI",
+            sequence="GAATTC",
         )
 
-    def test_presence_within_tolerance(
-        self, sars_cov_2_genome, mock_restriction_enzyme, tolerance=0.05
-    ):
-        actual = RestrictionEnzymePresence()(
-            sars_cov_2_genome, mock_restriction_enzyme, tolerance
-        )
+    def test_presence_within_tolerance(self, sars_cov_2_genome, ecori, tolerance=0.05):
+        actual = RestrictionEnzymePresence()(sars_cov_2_genome, ecori, tolerance)
+
         assert True == actual
 
-    def test_absence_within_tolerance(
-        self, sars_cov_2_genome, mock_restriction_enzyme, tolerance=0.025
-    ):
-        actual = RestrictionEnzymePresence()(
-            sars_cov_2_genome, mock_restriction_enzyme, tolerance
-        )
+    def test_absence_within_tolerance(self, sars_cov_2_genome, ecori, tolerance=0.025):
+        actual = RestrictionEnzymePresence()(sars_cov_2_genome, ecori, tolerance)
+
         assert False == actual
