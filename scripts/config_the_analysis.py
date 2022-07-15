d44494039e999f7fd8cb0b9cd2492115319b1058
Anna Julia Engel <anna.julia.engel@gmail.com>
2022-07-15 15:47:36 +0200

    Drop jupyter dev dependency

--- scripts/config_the_analysis.py
+++ scripts/config_the_analysis.py
@@ -1,13 +1,13 @@
 from ostruct import OpenStruct
 
-SARS_COV_2_ACCESSION = "MN908947.1"
+SARS_COV_2_ACCESSION_NAME = "MN908947.1"
 
 THE_CONFIG = OpenStruct(
     sars_cov_2_structure_diagram=OpenStruct(
         genes_bed_file="assets/bed/sars_cov_2_genes_for_diagram.bed",
-        title=f"SARS-CoV-2 (accession {SARS_COV_2_ACCESSION}) genetic structure",
+        title=f"SARS-CoV-2 (accession {SARS_COV_2_ACCESSION_NAME}) genetic structure",
     ),
     genome_under_test=OpenStruct(
-        fasta_file_path=f"assets/fasta/{SARS_COV_2_ACCESSION}.fasta"
+        fasta_file_path=f"assets/fasta/{SARS_COV_2_ACCESSION_NAME}.fasta"
     ),
 )
